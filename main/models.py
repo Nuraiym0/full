5cd718facaf1fe0a5d2c707e58c5541476de75c2
shahnazar <sahnazarborubaev9@gmail.com>
2023-01-16 17:14:45 +0600

    активация кода , восстановление пароля , приложение book ; main, swagger

--- main/models.py
+++ main/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    image = models.ImageField(upload_to='media', null=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    rating = models.IntegerField(default=0, help_text='Указывать рейтинг в integer')
+    cuisine = models.CharField(max_length=50)
+    work_time = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = "Ресторан"
+    
+class Post(models.Model):
+    title = models.CharField(max_length=70, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='media', null=True, verbose_name='Изображение')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+
+    TYPE = [
+        ('BRK', 'Завтрак'),
+        ('LUN', 'Обед'),
+        ('DIN', 'Ужин'),
+    ]
+
+    type = models.CharField(choices=TYPE, max_length=3, default='BRK', verbose_name='Тип')
+
+# class Subscription(models.Model):
+#     subscribe = models.ForeignKey(User, related_name='subscriptions', on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, related_name='subscribers', on_delete=models.CASCADE)
+
+        
+
+
+    
+    
--- main/views.py
+++ main/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from rest_framework.decorators import action
+from django.db.models import Q
+from rest_framework.response import Response
+
+from .serializers import RestaurantSerializer, PostSerializer
+from .models import Restaurant, Post
+from rest_framework.permissions import IsAdminUser
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+
+from rest_framework import filters
+
+class RestaurantViewSet(ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    @swagger_auto_schema(manual_parameters=[
+        openapi.Parameter('q', openapi.IN_QUERY, type=openapi.TYPE_STRING)
+    ])
+    @action(['GET'], detail=False)
+    def search(self, request):
+        q = request.query_params.get('q')
+        queryset = self.get_queryset() # Product.objects.all()
+        if q:
+            queryset = queryset.filter(Q(title__icontains=q))
+
+        pagination = self.paginate_queryset(queryset)
+        if pagination:
+            serializer = self.get_serializer(pagination, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+
+
