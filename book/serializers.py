920b36fc69099a6eac60f5bd715d16aad78015ec
shahnazar <sahnazarborubaev9@gmail.com>
2023-01-17 17:09:41 +0600

    book доделал

--- book/serializers.py
+++ book/serializers.py
@@ -2,45 +2,66 @@ from rest_framework import serializers
 
 from .models import User
 
-
-class RegisterUserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
     password_confirm = serializers.CharField(min_length=4, required=True)
+
+    class Meta: 
+        model = User
+        fields = ('email', 'password', 'password_confirm', 'first_name', 'last_name', 'phone',)
+    
+    def validate(self, attrs):
+        p1 = attrs.get('password')
+        p2 = attrs.pop('password_confirm')
+        if p1 != p2:
+            raise serializers.ValidationError("Passwords not match")
+        return attrs
+    
+    def validate_email(self, email):
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("User with email already exists")
+        return email
+
+    def validate_phone(self, phone):
+        if len(phone) > 13:
+            raise serializers.ValidationError("The number should be max 13 digits")
+        return phone
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+class CreateNewPasswordSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(max_length=150, required=True)
+    activation_code = serializers.CharField(max_length=8, min_length=8, required=True)
+    password = serializers.CharField(min_length=8, required=True)
+    password_confirm = serializers.CharField(min_length=8, required=True)
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'password_confirm')
-
-    def validate(self, attrs):
-        # attrs = {"email":"some@gmail.com", "password":"1234", "password_confirm":"1234"}
-        pass1 = attrs.get("password")
-        pass2 = attrs.pop("password_confirm")
-        if pass1 != pass2:
-            raise serializers.ValidationError("Passwords do not match")
-        return attrs
+        fields = ('email', 'password', 'password_confirm', 'activation_code')
 
     def validate_email(self, email):
-        # email = "some@gmail.com"
-        if User.objects.filter(email=email).exists():
-            raise serializers.ValidationError("User with this email already exists")
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Пользователя с таким email не найден')
         return email
 
-    def create(self, validated_data):
-        # validated_data = {"email":"some@gmail.com", "password":"1234", "password_confirm":"1234"}
-        return User.objects.create_user(**validated_data)
-
-class ForgotSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-
     def validate(self, attrs):
-        email = attrs.get('email')
-        try:
-            User.objects.get(email=email)
-        except User.DoesNotExist:
-            raise serializers.ValidationError('Such email does not found')
+        password = attrs.get('password')
+        password_confirm = attrs.get('password_confirm')
+        if password != password_confirm:
+            raise serializers.ValidationError('Пароли не совпадают')
         return attrs
-    
-    def save(self):
+
+    def save(self, **kwargs):
         data = self.validated_data
-        user = User.objects.get(**data)
-        user.set_activation_code()
-        user.password_confirm()
+        email = data.get('email')
+        password = data.get('password')
+        try:
+            user = User.objects.get(email=email)
+            if not user:
+                raise serializers.ValidationError('Пользователь не найден')
+        except User.DoesNotExist:
+            raise serializers.ValidationError('Пользователь не найден')
+
+        user.set_password(password)
+        user.save()
+        return user
