fbdf444f94b615fbb35208b72d62c0af485b3abe
Nuraiym0 <nuraiymmamytova0@gmail.com>
2023-02-20 18:27:47 +0600

    rest

--- config/settings.py
+++ config/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
 
     }
 }
